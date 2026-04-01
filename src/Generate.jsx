adaac990375d08181311a7a1e9ad1e9d18b8fad8
oliver2206 <oliveriinathanieltolentino@gmail.com>
2026-04-01 10:53:57 +0800

    Update Generate.jsx

--- src/Generate.jsx
+++ src/Generate.jsx
@@ -181,7 +181,6 @@ export default function Navbar({ activePage, onNavigate, onHome }) {
           <div className="navbar-logo-ring">🍀</div>
           <div>
             <div className="navbar-title">BINGO FORTUNE</div>
-            <div className="navbar-home-hint">↩ CLICK TO GO HOME</div>
           </div>
         </div>
 
